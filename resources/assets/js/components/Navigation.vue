5448976bd532721da0921a0913d3e633282d398a
nishanth-kd <kd@coloredcow.in>
2018-05-26 18:45:07 +0530

    Initial Commit

---
+++ resources/assets/js/components/Navigation.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="links">
+        <nav-item v-on:toggle-nav="toggleNav" v-bind:key="link.src" v-bind:src="link.src"  v-bind:text="link.text" v-for="link in links"></nav-item>
+    </div>
+</template>
+<script>
+export default {
+    props: ['links'],
+    methods: {
+        toggleNav () {
+            console.log('toggle-nav:navitgation');
+            this.$emit('toggle-nav');
+        }
+    }
+}
+</script>
---
+++ resources/assets/js/components/NavigationItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <a href="#" v-on:click="toggleNav" v-text="text"></a> 
+    <!-- v-bind:href="src" -->
+</template>
+<script>
+export default {
+    props: ['src', 'text'],
+    methods: {
+        toggleNav () {
+            console.log('toggle-nav:nav-item');
+            this.$emit('toggle-nav');
+        }
+    }
+}
+</script>
